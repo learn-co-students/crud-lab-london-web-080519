bcd2f34d672cdbd6177848390e99fe4921120a4d
oliburt <oliburt@gmail.com>
2019-11-19 18:44:47 +0000

    Done.

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -1,7 +1,63 @@
-
-import cuid from 'cuid';
+import ACTIONS from "./actions";
+import cuid from "cuid";
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state, action) {
+export default function manageRestaurants(
+  state = {
+    restaurants: [],
+    reviews: []
+  },
+  action
+) {
+  switch (action.type) {
+    case ACTIONS.ADD_RESTAURANT:
+      const restaurant = {
+        id: cuid(),
+        text: action.text
+      };
+      return {
+        ...state,
+        restaurants: [...state.restaurants, restaurant]
+      };
 
+    case ACTIONS.UPDATE_RESTAURANT:
+      return {
+        ...state,
+        restaurants: state.restaurants.map(rest => {
+          if (rest.id === action.payload.id) return action.payload;
+          return rest;
+        })
+      };
+
+    case ACTIONS.DELETE_RESTAURANT:
+      return {
+        ...state,
+        restaurants: state.restaurants.filter(rest => rest.id !== action.id)
+      };
+    case ACTIONS.ADD_REVIEW:
+      const review = {
+        id: cuid(),
+        restaurantId: action.review.restaurantId,
+        text: action.review.text
+      };
+      return { ...state, reviews: [...state.reviews, review] };
+
+    case ACTIONS.UPDATE_REVIEW:
+      return {
+        ...state,
+        reviews: state.reviews.map(rest => {
+          if (rest.id === action.payload.id) return action.payload;
+          return rest;
+        })
+      };
+
+    case ACTIONS.DELETE_REVIEW:
+      return {
+        ...state,
+        reviews: state.reviews.filter(rest => rest.id !== action.id)
+      };
+
+    default:
+      return state;
+  }
 }
